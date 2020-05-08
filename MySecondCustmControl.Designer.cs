0ea38f71d363a179cee4483227ec2aeabaf7d5fc
gamebred7 <54005369+gamebred7@users.noreply.github.com>
2020-05-08 19:40:43 +0300

    Add files via upload

---
+++ MySecondCustmControl.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace FastFoodDemo
+{
+    partial class MySecondCustmControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MySecondCustmControl));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(33, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(751, 367);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(377, 228);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Elephant", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(174)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(14, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(699, 103);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "SANATORIUM";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(387, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(343, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "A sanatorium is a medical facility for long-term";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(387, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "illness. It\'s an institution that provides";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(387, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(342, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "medical treatment and rest, often in a healthy";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(387, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(358, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "climate, for people who have been ill for a long";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(387, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(349, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "time. Our sanatorium is a well oxygened place,";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(387, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(260, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "generally full of trees with fresh air,";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(387, 265);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(322, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "to inhibit the patient of tuberculosis disease";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(387, 285);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(234, 20);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "for restoring health and vitality.";
+            // 
+            // MySecondCustmControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScrollMargin = new System.Drawing.Size(1, 1);
+            this.Controls.Add(this.panel1);
+            this.Name = "MySecondCustmControl";
+            this.Size = new System.Drawing.Size(817, 406);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.Common;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace FastFoodDemo
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            PopularTreatmentPrograms();
+        }
+
+        public void PopularTreatmentPrograms()
+        {
+            string provider = ConfigurationManager.AppSettings["provider"];
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                DbCommand command = factory.CreateCommand();
+
+                command.Connection = connection;
+                command.CommandText = "EXECUTE PopularTreatmentPrograms";
+
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    int n = 0;
+                    while (dataReader.Read() && n<3)
+                    {
+                        if (n == 0)
+                        {
+                            label1.Text = dataReader["ProgramName"].ToString();
+                            label4.Text = "$" + dataReader["ProgramPrice"].ToString();
+                            label19.Text = "Duration: "+dataReader["ProgramDuration"].ToString()+" days";
+                        }
+                        if (n == 1)
+                        {
+                            label12.Text = dataReader["ProgramName"].ToString();
+                            label11.Text = "$" + dataReader["ProgramPrice"].ToString();
+                            label21.Text = "Duration: "+dataReader["ProgramDuration"].ToString()+" days";
+                        }
+                        if (n == 2)
+                        {
+                            label8.Text = dataReader["ProgramName"].ToString();
+                            label7.Text = "$" + dataReader["ProgramPrice"].ToString();
+                            label20.Text = "Duration: "+dataReader["ProgramDuration"].ToString()+ " days";
+                        }
+                        n++;
+                    }
+                }
+            }
+        }
+    }
+}
